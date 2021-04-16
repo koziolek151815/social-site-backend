3b6d36fbc124edb582f6b44d94981a55bb6e404a
koziolek151815 <koziolek151815x@gmail.com>
2021-04-16 23:25:40 +0200

    Initial commit

---
+++ src/test/java/com/socialsitebackend/socialsite/SocialSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialsitebackend.socialsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
